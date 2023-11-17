193f9e38284d072abeb377e6139e62678f9d8509
Audun Rugstad <arugstad@gmail.com>
2023-11-17 10:56:49 +0100

    Update script_test.R
    make everything better

--- script_test.R
+++ script_test.R
@@ -8,6 +8,8 @@ library(dplyr)
 vector <- c(1,2,10)
 vector2 <- c(3,6,1)
 
-data.frame <- data.frame(vector,vector2)
+data.frame.dust <- data.frame(vector,vector2)
 
-new.data.frame <- data.frame
+new.data.frame <- data.frame.dust
+
+write.csv(new.data.frame, "auduns_supertabell.csv")
